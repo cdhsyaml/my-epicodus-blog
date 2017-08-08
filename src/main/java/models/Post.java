f81f256d020e77c1af4941dde56b0b1102002654
shy <shyamal.punekar@gmail.com>
2017-08-07 20:46:32 -0700

    Add post and test method for blog

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Created by ssaxe1 on 8/7/17.
+ */
+public class Post {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public Post(String content){
+        this.content = content;
+    }
+}
